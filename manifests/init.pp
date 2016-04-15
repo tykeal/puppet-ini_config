1cc19863bc791cd228f384ecdfe27250364d10d0
Andrew Grimberg <agrimberg@linuxfoundation.org>
2016-04-15 13:19:28 -0700

    Docify the module
    Add in all the puppet-strings doc tags for the module. Interestingly
    'rake strings:generate' is bombing and I can't figure out exactly why
    
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,7 +13,52 @@
 #
 # Copyright 2016 Andrew Grimberg
 #
-# @License Apache-2.0 <http://spdx.org/licenses/Apache-2.0>
+# License
+# -------
+#
+# Apache-2.0 <http://spdx.org/licenses/Apache-2.0>
+#
+# @example
+#   ini_config { '/etc/my_config.ini':
+#   }
+#
+# @example
+#   ini_config { 'My config':
+#     config_file => '/etc/my_config.ini',
+#   }
+#
+# @example
+#   ini_config { '/etc/my_config.ini':
+#     config              => {
+#       'testsection'     => {
+#         'testvar1'      => 'testvar1',
+#         'testvar2'      => [ 'testvar2.1', 'testvar2.2' ],
+#       },
+#       'testsection.sub' => {
+#         'testvar3'      => 'testvar3',
+#       },
+#     },
+#   }
+#
+# @param ensure Ensure if the configuration file is present or not
+#
+# @param config_file Absolute path to file. Default is to use title
+#
+# @param config The configuration hash
+#
+# @param mode Fully qualified octal file mode
+#
+# @param owner Owner of the configuraiton file
+#
+# @param group Group owner of the configuration file
+#
+# @param quotesubsection If ini subections (noted by .subsection in the
+#   configuration key) should be quoted or not. Some applications using ini
+#   style files require it and some can't handle it.
+#
+# @param indentoptions If the ini section options should be indented (by a tab)
+#   or not. Some applications using ini sytle configuration files can not handle
+#   leading white space
 #
 define ini_config (
   Enum['present', 'absent'] $ensure = 'present',
