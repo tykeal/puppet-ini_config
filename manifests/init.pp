c694e70e05c187591a24058c8c0c010e118018d7
Andrew Grimberg <agrimberg@linuxfoundation.org>
2016-04-15 10:54:43 -0700

    Build out ini_config define
    Module is a single define that uses the default init.
    
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

--- manifests/init.pp
+++ manifests/init.pp
@@ -16,7 +16,36 @@
 # @License Apache-2.0 <http://spdx.org/licenses/Apache-2.0>
 #
 define ini_config (
+  Enum['present', 'absent'] $ensure,
+  String $config_file      = '',
+  Hash $config             = {},
+  Pattern[/^[0-7]{4}$/] $mode = '0440',
+  String $owner            = 'root',
+  String $group            = 'root',
+  Boolean $quotesubsection = true,
+  Boolean $indentoptions   = true,
 ) {
+  # Our config file should be either $config_file or $title but it must be an
+  # absolute_path
+  if ($config_file == '') {
+    validate_absolute_path($title)
+    $file_path = $title
+  } else {
+    validate_absolute_path($config_file)
+    $file_path = $config_file
+  }
 
+  if ($ensure == 'present') {
+    $_ensure = 'file'
+  } else {
+    $_ensure = $ensure
+  }
 
+  file { $file_path:
+    ensure  => $_ensure,
+    owner   => $owner,
+    group   => $group,
+    mode    => $mode,
+    content => template("${module_name}/config.erb"),
+  }
 }
--- spec/defines/init_spec.rb
+++ spec/defines/init_spec.rb
@@ -1,9 +1,108 @@
 require 'spec_helper'
 describe 'ini_config', :type => :define do
-  let(:title) { 'test_config' }
+  let (:params) {
+    {
+      'ensure'            => 'present',
+      'config_file'       => '',
+      'config'            => {
+        'testsection'     => {
+          'testvar1'      => 'testvar1',
+          'testvar2'      => [ 'testvar2.1', 'testvar2.2' ],
+        },
+        'testsection.sub' => {
+          'testvar3'      => 'testvar3',
+        }
+      },
+      'mode'              => '0440',
+      'owner'             => 'foo',
+      'group'             => 'bar',
+      'quotesubsection'   => true,
+      'indentoptions'     => true,
+    }
+  }
 
-  context 'with defaults for all parameters' do
+  context 'with good params' do
+    let(:title) { '/foo/test.config' }
+
     it { should contain_ini_config(title) }
+    it { should contain_file(title).with(
+      :ensure  => 'file',
+      :owner   => params['owner'],
+      :group   => params['group'],
+      :mode    => params['mode'],
+      :content => '; MANAGED BY PUPPET
+
+[testsection]
+	testvar1 = testvar1
+	testvar2 = testvar2.1
+	testvar2 = testvar2.2
+
+[testsection "sub"]
+	testvar3 = testvar3
+
+',
+    ) }
+
+    it "should set a filename of params['config_file'] if passed" do
+      params.merge!({'config_file' => '/bar/test.ini'})
+      is_expected.to contain_file(params['config_file'])
+    end
+
+    it 'should not quote the subsection title if quotesubsection is false' do
+      params.merge!({'quotesubsection' => false})
+      is_expected.to contain_file(title).with(
+        :content => '; MANAGED BY PUPPET
+
+[testsection]
+	testvar1 = testvar1
+	testvar2 = testvar2.1
+	testvar2 = testvar2.2
+
+[testsection sub]
+	testvar3 = testvar3
+
+',
+      )
+    end
+
+    it 'should not indent section options if indentoptions is false' do
+      params.merge!({'indentoptions' => false})
+      is_expected.to contain_file(title).with(
+        :content => '; MANAGED BY PUPPET
+
+[testsection]
+testvar1 = testvar1
+testvar2 = testvar2.1
+testvar2 = testvar2.2
+
+[testsection "sub"]
+testvar3 = testvar3
+
+',
+      )
+    end
+
+    it "should have a config file marked as absent if ensure is 'absent'" do
+      params.merge!({'ensure' => 'absent'})
+      is_expected.to contain_file(title).with(
+        :ensure => 'absent',
+      )
+    end
+  end
+
+  context 'with bad title' do
+    let(:title) { 'badtitle' }
+
+    it "should throw an error if title and config_file do not have an absolute path" do
+      expect { should compile }.to \
+        raise_error(RSpec::Expectations::ExpectationNotMetError,
+          /is not an absolute path/)
+    end
+
+    it 'should have no problem if config_file has an absolute path' do
+      params.merge!({'config_file' => '/bar/conf.ini'})
+      is_expected.to contain_file(params['config_file'])
+    end
   end
 end
 
